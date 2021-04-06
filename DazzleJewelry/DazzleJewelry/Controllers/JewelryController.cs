77e024875267cd39de25f1ef56bc4c9179a1eb27
ezgih <ezgihareket@hotmail.com>
2021-04-06 10:39:52 +0300

    hafta4

---
+++ DazzleJewelry/DazzleJewelry/Controllers/JewelryController.cs
@@ -0,0 +1,34 @@
+﻿using DazzleJewelry.Models;
+using DazzleJewelry.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DazzleJewelry.Controllers
+{
+    public class JewelryController : Controller
+    {
+
+        private readonly IJewelryRepository _jewelryRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public JewelryController(IJewelryRepository jewelryRepository, ICategoryRepository categoryRepository)
+        {
+            _jewelryRepository = jewelryRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+
+            // return View(_jewelryRepository.GetAllJewelry);
+            var jewelryListViewModel = new JewelryListViewModel();
+            jewelryListViewModel.Jewelries = _jewelryRepository.GetAllJewelry;
+            jewelryListViewModel.CurrentCategory = "En Çok Satılanlar";
+            return View(jewelryListViewModel);
+            
+        }
+    }
+}
---
+++ DazzleJewelry/DazzleJewelry/ViewModels/JewelryListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DazzleJewelry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DazzleJewelry.ViewModels
+{
+    public class JewelryListViewModel
+    {
+        public IEnumerable<Jewelry> Jewelries { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
